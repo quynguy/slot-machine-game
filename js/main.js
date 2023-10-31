28a67b66db33f5445c195bedecf14b9f27811fec
wquynguy <wquyenle@gmail.com>
2023-10-31 11:21:09 -0500

    Tuesday Mid Morning HTML/CSS/JS Update

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log("js:loaded")
+
+const balanceDisplayEl = document.getElementById("balance-stat");
+let currentBalance = 0;
+let maxBalance = 10000;
+
+// console.log(currentBalance);
+
+function balanceDisplay() {
+    return document.getElementById("balance-stat");
+}
+
+const creditDisplayEl = document.getElementById("credit-stat");
+let currentCredit = 0;
+let maxCredit = 10000;
+
+// console.log(creditDisplayEl);
+
+function creditDisplay() {
+    return document.getElementById("credit-stat");
+}
+
+// console.log(maxCredit);
+
+
+
+
+
+
+
+
+
+
+
