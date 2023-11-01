d1c87cc67456e7747ec95c0218964da3a620ed4d
wquynguy <wquyenle@gmail.com>
2023-11-01 18:25:42 -0500

    Attempt DRY update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 // * variables* // 
 
-let balance = 500; 
-let currentBet = 25;
+let credit = 0; 
+let balance = 0;
 
-
-const winningCombinations = {
-    ghost: 100,
-    witch: 250,
-    cat: 300,
-};
 
 const symbolsArray = [];
 symbolsArray[0]= "Ghost";
@@ -36,13 +30,56 @@ const betMax = document.getElementById("btnMax");
 
 
 // * functions * // 
-function updateStats() {
-    creditStat.textContent = `$ ${balance.toFixed(2)}`;
-    balanceStat.textContent = `$ ${(balance - currentBet).toFixed(2)}`;
+function updateCredits() {
+    creditStat.textContent = `$ ${credit.toFixed(2)}`;
 }
 
-// console.log(gameResults);
+let betAmt25 = 25;
+let betAmt50 = 50;
+let maxBet = 100;
 
+const betAmount = [betAmt25, betAmt50, maxBet];
+
+const won25= 20;
+const won50= 50;
+const wonMax= 100;
+
+
+function updateBalance(winningCombo, betAmount) {
+    if (winningCombo > 0) {
+        balance += winningCombo;
+    } else {
+        credit -= betAmount;
+    }
+    balanceStat.textContent = `$ ${balance.toFixed(2)}`;
+}
+
+
+function checkCombo(spinResult, betAmount) {
+    const winningCombinations = {
+        ghost: 100,
+        witch: 250,
+        cat: 300,
+    };
+
+    let totalWinnings = 0;
+
+    if (totalWinnings > 0) {
+        updateBalance(totalWinnings);
+    } else {
+        updateBalance(betAmount);
+    }
+
+    for (const symbols of spinResult){
+        if (winningCombinations[symbols]) {
+            totalWinnings += winningCombinations[symbols];
+        }
+    }
+
+    console.log('spinResult:', spinResult);
+    console.log('totalWinnings:', totalWinnings);
+    console.log('betAmount:', betAmount);
+}
 
 // * event listeners * //
 
@@ -50,8 +87,8 @@ depositForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const depositInput = parseFloat(document.getElementById("deposit").value);
     if (!isNaN(depositInput) && depositInput > 0) {
-        balance += depositInput;
-        updateStats();
+        credit += depositInput;
+        updateCredits();
     }
 });
 
@@ -64,10 +101,24 @@ spinButton.addEventListener("click", function () {
     gameResults.push(randomSymbols);
 }
     console.log(gameResults);
+    checkCombo(gameResults, betAmount);
 });
+
+
 
 cashOut.addEventListener("click", function () {
     alert("Thanks for playing!");
 });
 
 
+bet25btn.addEventListener("click", function () {
+    checkCombo(gameResults, betAmt25);
+});
+
+bet50btn.addEventListener("click", function () {
+    checkCombo(gameResults, betAmt50);
+});
+
+betMax.addEventListener("click", function (){
+    checkCombo(gameResults, maxBet);
+});
